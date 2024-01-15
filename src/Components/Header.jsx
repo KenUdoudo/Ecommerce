2779ba69414c634ffd0723f6b610ddf43927e312
Ken Udoudo <kendo302003@gmail.com>
2024-01-14 19:15:40 -0800

    LIST OF ICONS

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,35 @@ import { HiHome,
     HiMagnifyingGlass,
     HiStar,
     HiPlayCircle,
-    HiTv } from "react-icons/hi2"
+    HiTv, 
+    HiPlus} from "react-icons/hi2"
 function Header() {
+  const menu=[
+    {
+      name: 'HOME',
+      icon:HiHome
+    },
+    {
+      name: 'SEARCH',
+      icon:HiMagnifyingGlass
+    },
+    {
+      name: 'WATCH LIST',
+      icon:HiPlus
+    },
+    {
+      name: 'ORIGINALS',
+      icon:HiStar
+    },
+    {
+      name: 'MOVIES',
+      icon:HiPlayCircle
+    },
+    {
+      name: 'SERIES',
+      icon:HiTv
+    },
+  ]
   return (
     <div>
       <img src={ logo } className='w-[80px] md:w-[115px] object-cover'/>
