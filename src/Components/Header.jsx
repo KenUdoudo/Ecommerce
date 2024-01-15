e241218219bbb6bdc0236e404a515ac75274ddd5
Ken Udoudo <kendo302003@gmail.com>
2024-01-14 19:03:19 -0800

    logo styling

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import logo from './../assets/Disney-Plus.png'
 function Header() {
   return (
-    <div>Header</div>
+    <div>
+      <img src={ logo } className='w-[80px] object-cover'/>
+    </div>
   )
 }
 
