5c4f0382cec138ccb549feabb17f1625ff865e76
Ken Udoudo <kendo302003@gmail.com>
2024-01-14 19:09:06 -0800

    importing icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import logo from './../assets/Disney-Plus.png'
+import { HiHome,
+    HiMagnifyingGlass,
+    HiStar,
+    HiPlayCircle,
+    HiTv } from "react-icons/hi2"
 function Header() {
   return (
     <div>
-      <img src={ logo } className='w-[80px] object-cover'/>
+      <img src={ logo } className='w-[80px] md:w-[115px] object-cover'/>
     </div>
   )
 }
