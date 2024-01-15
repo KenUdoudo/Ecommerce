f780c7861c2d0f159f10af3f936851f2cc0a2919
Ken Udoudo <kendo302003@gmail.com>
2024-01-14 20:38:55 -0800

    flex

--- src/Components/HeaderItem.jsx
+++ src/Components/HeaderItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeaderItem({name,Icon}) {
   return (
-    <div>
+    <div className='font-[600] flex items-center gap-3 text-[18px] cursor-pointer'>
         <Icon />
         <h2>{name}</h2>
     </div>
