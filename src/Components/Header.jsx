f1ef560eb4df7c5c815d66e70cef0b7f157e7d87
Ken Udoudo <kendo302003@gmail.com>
2024-01-14 21:07:34 -0800

    omo

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,8 +4,8 @@ import { HiHome,
     HiMagnifyingGlass,
     HiStar,
     HiPlayCircle,
-    HiTv, 
-    HiPlus} from "react-icons/hi2"
+    HiTv} from "react-icons/hi2";
+import { HiPlus, HiDotsVertical } from 'react-icons/hi';
 import HeaderItem from './HeaderItem'
 function Header() {
   const menu=[
@@ -43,10 +43,18 @@ function Header() {
         <HeaderItem name={item.name} Icon={item.icon}/>
       ))}
       </div>
-      <div className='flex items-center gap-8'>
-      {menu.map((item)=>(
+      <div className='flex md:hidden items-center gap-5'>
+      {menu.map((item,index)=>index<3&&(
+        <HeaderItem name={''} Icon={item.icon}/>
+      ))}
+      <div className='md:hidden'>
+        <HeaderItem name={''} Icon={HiDotsVertical}/>
+        <div className='absolute mt-3 bg-slate-100 border-[1px] p-3'>
+        {menu.map((item,index)=>index>3&&(
         <HeaderItem name={item.name} Icon={item.icon}/>
       ))}
+        </div>
+      </div>
       </div>
       </div>
       <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" className='w-[40px] rounded-full'/>
--- src/Components/HeaderItem.jsx
+++ src/Components/HeaderItem.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function HeaderItem({name,Icon}) {
   return (
-    <div className='font-[600] flex items-center gap-3 text-[15px] cursor-pointer hover:underline underline-offset-8'>
+    <div className='font-[600] flex items-center gap-3 text-[15px] cursor-pointer hover:underline underline-offset-8 mb-3'>
         <Icon />
-        <h2 className='hidden md:block'>{name}</h2>
+        <h2 className=''>{name}</h2>
     </div>
   )
 }
