9a398cc28caed219ed4861aed72e286d9a5d1c82
Ken Udoudo <kendo302003@gmail.com>
2024-01-17 04:11:38 -0800

    done with get

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -2,3 +2,7 @@ import axios from "axios";
 
 
 const movieBaseUrl='https://api.themoviedb.org/3'
+const api_key='3d85ed8a3515b32d60c40a73ecac30b7'
+
+
+const getTrendingVideos=axios.get(this.movieBaseUrl+"/trending/all/day?api_key"+api_key);
