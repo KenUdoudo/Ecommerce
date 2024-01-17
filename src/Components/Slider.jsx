fc06e007d1f0bf2898dbc5f95ac46b87a2dc5fdb
Ken Udoudo <kendo302003@gmail.com>
2024-01-17 09:54:17 -0800

    fixed

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -1,21 +1,24 @@
 import React, { useEffect } from 'react'
 import GlobalApi from '../Services/GlobalApi'
-
+import { useState } from 'react';
 function Slider() {
 
-   const [movieList,setMovieList]=useState([]);
+  const [movieList,setMovieList]=useState([]);
     useEffect(()=>{
-        getTrendingMovies()
+        getTrendingMovies();
     },[])
 
     const getTrendingMovies=()=>{
         GlobalApi.getTrendingVideos.then(resp=>{
             console.log(resp.data.results);
             setMovieList(resp.data.results)
+            
         })
     }
   return (
-    <div>Slider</div>
+    <div>
+      
+    </div>
   )
 }
 
