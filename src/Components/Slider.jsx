cbec0bc6eb733a2cc84c09ca68773176c86decb1
Ken Udoudo <kendo302003@gmail.com>
2024-01-18 10:00:17 -0800

    border

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -3,6 +3,7 @@ import GlobalApi from '../Services/GlobalApi'
 import { useState } from 'react';
 import { HiChevronLeft, HiChevronRight } from 'react-icons/hi2';
 const IMAGE_BASE_URL="http://image.tmdb.org/t/p/original/";
+const screenWidth=window.innerWidth;
 function Slider() {
 
   const [movieList,setMovieList]=useState([]);
@@ -20,10 +21,10 @@ function Slider() {
     }
 
     const sliderRight=(element)=>{
-      element.scrollLeft+=800
+      element.scrollLeft+=screenWidth-110
     }
     const sliderLeft=(element)=>{
-      element.scrollLeft-=800
+      element.scrollLeft-=screenWidth-110
     }
   return (
     <div>
@@ -32,7 +33,7 @@ function Slider() {
 
     <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-none scroll-smooth' ref={elementRef}>
       {movieList.map((item,index)=>(
-        <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md'/>
+        <img src={IMAGE_BASE_URL+item.backdrop_path} className='min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in '/>
       ))}
     </div>
     </div>
